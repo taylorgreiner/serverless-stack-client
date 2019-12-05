ab4b9c7757af6f6fb14e8bd82c82bd7c8d652e38
Taylor Greiner <taylor.greiner@gmail.com>
2019-12-05 15:56:51 -0600

    Setting up the react app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "notes-app-uploads-123987"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://ll1u9x71u7.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_pgPmannL8",
+      APP_CLIENT_ID: "48rvg700c5rrv2ov7cqq6nsmbf",
+      IDENTITY_POOL_ID: "us-east-1:349a4bda-3d39-42d4-8e51-a2985680e34d"
+    }
+  };
